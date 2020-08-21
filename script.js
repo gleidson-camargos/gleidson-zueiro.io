84b13355ff2fe72802db08750247d2826412a62c
Gleidson Camargos <gleidsoncamargosnet@gmail.com>
2020-08-21 19:01:18 -0300

    adicionado todo projeto

---
+++ script.js
@@ -0,0 +1,50 @@
+function relogio() {
+    let data = new Date();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    let segundo = data.getSeconds();
+    let res = document.getElementById('relogio');
+
+    if (segundo < 10) {
+        segundo = `0` + segundo;
+    } if (minuto < 10) {
+        minuto = `0` + minuto;
+    } if (hora < 9) {
+        hora = `0` + hora;
+    }
+
+    let relog = `${hora}:${minuto}:${segundo}`;
+
+    res.innerHTML = relog;
+
+}
+setInterval(relogio, 1000);
+
+function tsuna() {
+
+    let mudas = document.getElementById('muda');
+    if (mudas.src = "img/3.png") {
+        mudas.src = "img/3.2.gif";
+    } else {
+        mudas.src = "/img/3.2.gif"
+
+    }
+
+}
+function mudawalter() {
+    let mudas = document.getElementById('mudawalter');
+
+    mudas.src = "img/4.1.gif";
+
+}
+function mudawalter2() {
+    let mudas = document.getElementById('mudawalter');
+    mudas.src = "img/4.jpg"
+
+}
+setInterval(mudawalter, 5000)
+setInterval(mudawalter2, 10000)
+
+
+
+
